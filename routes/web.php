44af02eefc1739e6f08cc72310c4f5e59a787d7b
Osama Assaf <96818454+OsamaAssaf@users.noreply.github.com>
2025-01-15 21:03:49 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Models\Book;
+use App\Models\Review;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $books = Book::popular('2023-01-01', '2023-12-31')->get();
+
+    return $books;
 });
